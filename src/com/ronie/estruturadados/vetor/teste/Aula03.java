062f330d78f353796e9dac42e770b2f60a30be3f
ronie <ronie.sousavieira@gmail.com>
2021-06-15 22:19:04 -0300

    add element of end matrix

---
+++ src/com/ronie/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.ronie.estruturadados.vetor.teste;
+
+import com.ronie.estruturadados.vetor.Vetor;
+// adicionar elementos no fim do vetor
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(2);
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+
+	}
+
+}
